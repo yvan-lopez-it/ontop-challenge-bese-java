50f65767610e5f590e008365598176d3d4456524
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-08-30 21:36:10 -0500

    init commit

---
+++ src/main/java/com/ontop/challenge/backend/apirest/BackendApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.ontop.challenge.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiRestApplication.class, args);
+    }
+
+}
