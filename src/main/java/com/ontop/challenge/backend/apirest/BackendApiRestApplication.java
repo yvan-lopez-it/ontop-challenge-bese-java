a4bb69486d7328b9e9f1d314acf727aefc2c43b8
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-09-02 18:32:33 -0500

    rest template config.

--- src/main/java/com/ontop/challenge/backend/apirest/BackendApiRestApplication.java
+++ src/main/java/com/ontop/challenge/backend/apirest/BackendApiRestApplication.java
@@ -2,9 +2,16 @@ package com.ontop.challenge.backend.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BackendApiRestApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApiRestApplication.class, args);
